eb0bc74eb7aa55d5a350f85924ce33a79637a101
prithvivarma-nallapuraju <nvpprithvivarma@gmail.com>
2023-07-24 15:04:50 +0530

    final commit of completion for Day1

--- Scripts/Day1/GetHomePageTitle.js
+++ Scripts/Day1/GetHomePageTitle.js
@@ -11,7 +11,6 @@ const getHomePageTitle = async (brwsr, drvr) => {
         await driver.manage().window().maximize()
         await driver.get('http://live.techpanda.org/')
         
-        console.log(`at getHomePageTitle line number 14 is`, process.env.CAPTURE_SCREENSHOTS)
         if(process.env.CAPTURE_SCREENSHOTS == 'true'){
             screenshotPath = mainpath.join(__dirname, `../../Outputs/${brwsr}/Day1`,'home_page.png')
             await screenshot(driver, screenshotPath)
